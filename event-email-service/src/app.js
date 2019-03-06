579eea8b3c501b9deccdae7d6398af4bc0c2874c
Joerg Bastian <github@joergbas.de>
2019-03-06 16:39:22 +0100

    Update app.js

--- event-email-service/src/app.js
+++ event-email-service/src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 //const tracing = require('./tracing.js');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
